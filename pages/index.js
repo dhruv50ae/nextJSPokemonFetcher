dc1d4526f8347290a5fe80b3fa2c3aa5f05c0d6a
dhruv50ae <linkdp2012@gmail.com>
2022-06-20 19:46:21 +0100

    ClientSideRendering
    Client side rendering means sending a request everytime something is clicked like a pokemon. This is evident by the page initially loading up null and then chaning that null state after the request send was successful. Different from serverside rendering and static site generation where they would have initial content which is the key factor from telling the three apart

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   const [pokemon, setPokemon] = useState([]);
@@ -18,7 +19,22 @@ export default function Home() {
       <Head>
         <title>Pokemon List</title>
       </Head>
-      <div>{JSON.stringify(pokemon)}</div>
+      <h2>Pokemon List</h2>
+      <div className={styles.grid}>
+        {pokemon.map((pokemon) => (
+          <div className={styles.card} key={pokemon.id}>
+            <Link href={`/pokemon/${pokemon.id}`}>
+              <a>
+                <img
+                  src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+                  alt={pokemon.name}
+                />
+                <h3>{pokemon.name}</h3>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
